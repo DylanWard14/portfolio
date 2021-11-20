bdacc8e7993352dde36ffd9be39df15f874ee85f
DylanWard14 <dylan.ward@iinet.net.au>
2021-11-20 13:09:38 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+  ],
+};
