fd5371bdff5950704fd908e77fe86a96d03db8cd
DylanWard14 <dylan.ward@iinet.net.au>
2021-11-20 14:36:11 +1000

    Initial setup

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./HomePage.scss";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <div>This will be the bio</div>
+      <div>This will be the expirence</div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import HomePage from "./HomePage/HomePage";
+
+import "./reset.scss";
+import "./index.scss";
+
+const IndexPage = () => {
+  return (
+    <>
+      <header>Dylan Ward</header>
+      <main>
+        <HomePage />
+      </main>
+    </>
+  );
+};
+
+export default IndexPage;
