a42aa17a440d99b29264f12b14498f8c7e17ed69
DylanWard14 <dylan.ward@iinet.net.au>
2021-11-28 10:57:23 +1000

    Connected contact form to emailjs

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,33 +1,74 @@
 import React from "react";
 import classNames from "classnames/bind";
+import emailjs from "emailjs-com";
 
 import * as styles from "./Contact.module.scss";
 
 export const Contact = () => {
+  const formRef = React.useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = React.useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const serviceId = process.env.GATSBY_CONTACT_SERVICE_ID;
+      const templateId = process.env.GATSBY_CONTACT_TEMPLATE_ID;
+      const userId = process.env.GATSBY_CONTACT_USER_ID;
+
+      if (formRef.current && serviceId && templateId && userId) {
+        emailjs.sendForm(serviceId, templateId, formRef.current, userId).then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      }
+    },
+    [formRef]
+  );
+
   return (
     <div className={styles.component}>
       <h2 className={styles.heading}>Contact</h2>
-      <form>
+      <form ref={formRef} onSubmit={sendEmail}>
         <label>
           Full Name
-          <input className={styles.input} />
+          <input
+            className={styles.input}
+            type="text"
+            name="from_name"
+            required
+          />
         </label>
         <label>
           Email
-          <input className={styles.input} />
+          <input
+            className={styles.input}
+            type="text"
+            name="user_email"
+            required
+          />
         </label>
         <label>
           Subject
-          <input className={styles.input} />
+          <input
+            className={styles.input}
+            type="text"
+            name="message_subject"
+            required
+          />
         </label>
         <label>
           Message
-          <textarea className={classNames(styles.input, styles.message)} />
+          <textarea
+            className={classNames(styles.input, styles.message)}
+            name="message"
+            required
+          />
         </label>
-        <button
-          className={styles.submit}
-          onClick={(event) => event.preventDefault()}
-        >
+        <button className={styles.submit} type="submit" value="Send">
           Send Message
         </button>
       </form>
